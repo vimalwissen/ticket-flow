0e3ce24589aa8c0cdf244bbe79a6842ec98c0fdb
vimalwissen <vimal.verma@wissen.com>
2025-11-20 13:15:09 +0000

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  root 'pages#index'
-   namespace :api do
+  namespace :api do
     namespace :version1 do
-      resources :tickets, param: :ticket_id  
-      
+      resources :tickets, param: :ticket_id
       get 'dashboard/summary', to: 'dashboard#summary'
     end
   end
-   get '*path', to: 'pages#index', via: :all
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Default: return a simple JSON response when hitting unmatched routes
+  get '*path', to: proc { [200, { 'Content-Type' => 'application/json' }, [{ message: 'API Running' }.to_json]] }
 end
