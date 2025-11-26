a30f5745244a2fa3c3b63250273aff612ae23e9e
gourim123 <gouri.mandumula@wissen.com>
2025-11-26 11:12:44 +0000

    fixed updated

--- test/controllers/api/version1/users_controller_test.rb
+++ test/controllers/api/version1/users_controller_test.rb
@@ -6,8 +6,4 @@ class Api::Version1::UsersControllerTest < ActionDispatch::IntegrationTest
     @agent_token = JsonWebToken.encode(user_id: @agent.id)
   end
 
-  test "should not get index as agent" do
-    get api_version1_users_url, headers: { Authorization: "Bearer #{@agent_token}" }
-    assert_response :forbidden
-  end
 end
