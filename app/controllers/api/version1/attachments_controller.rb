5c3a9acea67386aa642cdb89828f3cb2b7c8238f
Rohan <rohang.iitp@gmail.com>
2025-11-28 11:42:37 +0530

    attachment migration add

--- app/controllers/api/version1/attachments_controller.rb
+++ app/controllers/api/version1/attachments_controller.rb
@@ -1,14 +1,13 @@
-class AttachmentsController < ApplicationController
+class Api::Version1::AttachmentsController < ApplicationController
   def create
     ticket = Ticket.find(params[:ticket_id])
 
-    # Replace previous attachment if exists
     ticket.attachment.purge if ticket.attachment.attached?
 
     ticket.attachment.attach(params[:attachment])
 
     if ticket.save
-      render json: { message: "Attachment uploaded", ticket_id: ticket.id }, status: :created
+      render json: { message: "Attachment uploaded" }, status: :created
     else
       render json: { errors: ticket.errors.full_messages }, status: :unprocessable_entity
     end
