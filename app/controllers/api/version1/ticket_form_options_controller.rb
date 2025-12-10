2389d77b90ad88b7c513e683c85a9c2a27d5bd02
Vimal Verma <vimal.verma@wissen.com>
2025-12-10 10:42:38 +0000

    changed the ticket form options fields

--- app/controllers/api/version1/ticket_form_options_controller.rb
+++ app/controllers/api/version1/ticket_form_options_controller.rb
@@ -4,13 +4,6 @@ module Api
       # GET /ticket_form_options
       def index
         render json: {
-          status: [
-            { label: "Open", value: "open" },
-            { label: "In Progress", value: "in_progress" },
-            { label: "Resolved", value: "resolved" },
-            { label: "On Hold", value: "on_hold" },
-            { label: "Closed", value: "closed" }
-          ],
           priority: [
             { label: "Low", value: "low" },
             { label: "Medium", value: "medium" },
@@ -21,7 +14,22 @@ module Api
             { label: "Phone", value: "phone" },
             { label: "Web", value: "web" },
             { label: "Chat", value: "chat" }
-          ]
+          ],
+          status_transitions: {
+            admin: {
+              open: [ "in_progress", "on_hold", "resolved" ],
+              in_progress: [ "resolved", "on_hold" ],
+              on_hold: [ "in_progress", "resolved" ],
+              resolved: [ "open", "closed" ],
+              closed: [ "open" ]
+            },
+            agent: {
+              open: [ "in_progress", "on_hold", "resolved" ],
+              in_progress: [ "resolved", "on_hold" ],
+              on_hold: [ "in_progress", "resolved" ],
+              resolved: [ "closed" ]
+            }
+          }
         }, status: :ok
       end
     end
